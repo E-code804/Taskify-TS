e755fcb9e7b83d572dc54ccc4f28dfaf126566a2
Erik Pfeffer <epfeffer@terpmail.umd.edu>
2024-06-30 21:15:49 -0400

    Completed input bar, need to handle displaying of the todo list.

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import InputField from "./components/InputField";
+import { Todo } from "./models/models";
+
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
+      setTodo("");
+    }
+  };
+
+  console.log(todos);
+
+  return (
+    <div className="App">
+      <h1 className="heading">Taskify</h1>
+
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      {todos.map((t, idx) => (
+        <li key={idx}>{t.todo}</li>
+      ))}
+    </div>
+  );
+};
+
+export default App;
